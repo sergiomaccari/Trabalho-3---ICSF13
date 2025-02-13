2d75f7c81738159acd07ff4b9067ae0ed671ba23
sergiomaccari <sergio.r.maccari@gmail.com>
2025-02-13 17:55:28 -0300

    Update and rename trabalho3.h to trabalho3.c
    O que a gente fez na aula do dia 13/02

--- trabalho3.c
+++ trabalho3.c
@@ -10,35 +10,14 @@
 /* Leyza E. B. Dorini
 /*============================================================================*/
 
-#ifndef __TRABALHO3_H
-#define __TRABALHO3_H
-#include <stdlib.h>
-/*============================================================================*/
-
 #include "imagem.h"
-
-#define BRANCO 255
-#define PRETO 0
-
-/*============================================================================*/
-
-typedef struct
-{
-    int x;
-    int y;
-} Coordenada;
-
-/* Um tipo simples, usado para representar um retângulo envolvente. */
-typedef struct
-{
-    Coordenada tl; // top-left.
-    Coordenada br; // bottom-right
-} Rect;
+#include "trabalho3.h"
 
 /* Função central do trabalho. */
 Rect* encontraFuros(Imagem1C* mask, int* n_furos)
 {
     int i, j;
+    int min_x = 0, max_x = 0, min_y = 0, max_y = 0;
     int largura = mask->largura, altura = mask->altura;
     *n_furos = 0;
 
@@ -49,7 +28,7 @@ Rect* encontraFuros(Imagem1C* mask, int* n_furos)
         matriz_checagem[i] = (int*)malloc(largura * sizeof(int));
         for (j = 0; j < largura; j++)
         {
-            matriz_checagem[i][j] = 0;
+            matriz_checagem[i][j] = mask->dados[i][j];
         }
     }
 
@@ -91,54 +70,43 @@ Rect* encontraFuros(Imagem1C* mask, int* n_furos)
 
     // Preenche o vetor de retangulos
     int cont_retangulo = 0;
-    for (i = 0; i < altura; i++)
-    {
-        for (j = 0; j < largura; j++)
+    int availablePath = 1, symbol = 2, symbolAttrib = 0; lref,cref,lfinal,cdir,cesq;
+    while(avaliablePath){
+        for (i = 0; i < altura; i++)
         {
-            if (mask->dados[i][j] == BRANCO && matriz_checagem[i][j] == 0)
+            for (j = 0; j < largura; j++)
             {
-                // Definição dos limites do retangulo
-                int min_x = j, max_x = j, min_y = i, max_y = i;
-
-                //Talvez seria legal ou necessário colocar um condicional dentro de cada while para que ele nao extrapole os limites da imagem
-
-                // Aumenta o limite para a direita
-                while (mask->dados[i][max_x] == BRANCO)
-                {
-                    max_x++;
+                if (matriz_checagem[i][j] && !symbolAttrib){
+                    matriz_checagem[i][j] = symbol;
+                    symbol++;
+                    symbolAttrib = 1;
+                    lref = i;
+                    cref = j;
                 }
+                // vetorretangulo[symbol-2].cord.mmx
 
-                // Aumenta o limite para a esquerda
-                while (mask->dados[max_y][j] == BRANCO)
-                {
-                    max_y++;
+                if(matriz_checagem[i + 1][j]){
+                    matriz_checagem[i + 1][j] = symbol;
+
                 }
-
-                // // Aumenta o limite para baixo. Não é necessário aumentar o limite para cima devido como o loop percorre a amtrizICSF12
-                while (mask->dados[i][min_x - 1] == BRANCO)
-                {
-                    min_x--;
+                if(matriz_checagem[i][j + 1]){
+                    matriz_checagem[i + 1][j] = symbol;
+                    cdir++;
                 }
-
-                while (mask->dados[min_y - 1][j] == BRANCO)
-                {
-                    min_y--;
+                if(matriz_checagem[i][j - 1]){
+                    matriz_checagem[i + 1][j] = symbol;
+                    cesq++;
                 }
-
-                vetor_de_retangulos[cont_retangulo].tl.x = min_x;
-                vetor_de_retangulos[cont_retangulo].tl.y = min_y;
-                vetor_de_retangulos[cont_retangulo].br.x = max_x - 1;
-                vetor_de_retangulos[cont_retangulo].br.y = max_y - 1;
-
-                for (int y = min_y; y <= max_y; y++)
-                {
-                    for (int x = min_x; x <= max_x; x++)
-                    {
-                        matriz_checagem[y][x] = 1;
-                    }
+                if(matriz_checagem[i - 1][j]){
+                    matriz_checagem[i + 1][j] = symbol;
+                    lfinal++;
                 }
-
-                cont_retangulo++;
+                //coordenadas que seriam mandadas para o retangulo!, de preferencia deixar no ultimo loop do furo que ta sendo usado;
+                //ponto minimo = lref, cref- cesq;
+                //ponto maximo = lref+lfnial. cref+cdis
+                else{
+                    symbolAttrib = 0;
+                }
             }
         }
     }
@@ -152,4 +120,3 @@ Rect* encontraFuros(Imagem1C* mask, int* n_furos)
     return vetor_de_retangulos;
 }
 
-#endif /* __TRABALHO3_H */
